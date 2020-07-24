bb728a08daa1713cedb707b235bfd3e4a31f3b09
Chantel Yip <chantelyip422@gmail.com>
2020-07-23 20:16:58 -0700

    initial commit

---
+++ balanced_parantheses.py
@@ -0,0 +1,6 @@
+    """
+    Given a string, return True or False depending on whether that string has balanced parentheses.
+    For the purposes of this problem, you only need to worry about parentheses (( and )), not other opening-and-closing marks, like curly brackets, 
+    square brackets, or angle brackets.
+    """
+
