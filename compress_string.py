9a04c3d55cd726b459c2bddc595c7bcec5d0ad2f
Chantel Yip <chantelyip422@gmail.com>
2020-07-28 11:12:57 -0700

    2 success 1 failure

--- compress_string.py
+++ compress_string.py
@@ -5,8 +5,7 @@ times it repeats. If a character appears once, it should not be followed by a nu
 The function should handle letters, whitespace, and punctuation.
 
 >>> compress('aabbaabb')
-'a2b2a2b2'
-If a character appears once, it should not be followed by a number:
+'a2b2a2b2' 
 >>> compress('abc')
 'abc' 
 >>> compress('Hello, world! Cows go moooo...')
@@ -17,9 +16,31 @@ If a character appears once, it should not be followed by a number:
 def compress(string):
     """Return a compressed version of the given string."""
 
+    compressed = []
+    current = ""
+    count = 0
+
+    for char in string:
+        if char != current:
+            compressed.append(current)
+
+            if count > 1:
+                compressed.append(str(int(count)))
+
+            current = char
+            count = 0
+
+        count += 1
+
+    compressed.append(current)
+    if count > 1:
+        compressed.append(str(int(count)))
+
+    return "".join(compressed)
+
 
 if __name__ == "__main__":
     import doctest
-
-    if doctest.testmod().failed == 0:
-        print("\n✨ ALL TESTS PASSED!\n")
+result = doctest.testmod()
+if doctest.testmod().failed == 0:
+    print("\n✨ ALL TESTS PASSED!\n")
