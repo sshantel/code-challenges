d1523d72a20eca40a1669f8160f1a119f7149793
Chantel Yip <chantelyip422@gmail.com>
2020-08-03 12:22:43 -0700

    completed challenge

--- pig_latin.py
+++ pig_latin.py
@@ -9,8 +9,32 @@ You should turn this into the same phrase in Pig Latin.
 >>> pig_latin('hello awesome programmer')
 'ellohay awesomeyay rogrammerpay'
 """
+
+
 def pig_latin(phase):
     """Turn a phase into pig latin.
 
     There will be no uppercase letters or punctuation in the phrase.
+    """
+    pig_latin_convert = ""
+    vowels = "aeiou"
 
+    split_phase = phase.split()
+
+    for word in split_phase:
+        if word[0] in vowels:
+            word = word + "yay" + " "
+            pig_latin_convert += word
+
+        else:
+            word = word[1:] + word[0] + "ay" + " "
+            pig_latin_convert += word
+
+    return pig_latin_convert.strip()
+
+
+if __name__ == "__main__":
+    import doctest
+result = doctest.testmod()
+if doctest.testmod().failed == 0:
+    print("\n✨ ALL TESTS PASSED!\n")
