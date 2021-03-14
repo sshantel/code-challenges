67c8eb6d2f763dc9b28e2551bf51b5bbdee1938c
Chantel Yip <chantelyip422@gmail.com>
2021-03-14 00:05:50 -0800

    passed test cases

---
+++ array_left_rotations.py
@@ -0,0 +1,21 @@
+"""
+A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
+
+Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+ 
+rotLeft has the following parameter(s):
+
+int a[n]: the array to rotate
+int d: the number of rotations 
+
+int a'[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the number of left rotations.
+The second line contains  space-separated integers, each an a[i].
+
+"""
+def rotLeft(a, d):
+    return a[d:] + a[:d]
+
+
