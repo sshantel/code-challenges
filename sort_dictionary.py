e50ef16469b835bc8c6a870ae9b7576ed76fb60e
Chantel Yip <chantelyip422@gmail.com>
2021-03-14 18:59:48 -0700

    added parameters

---
+++ sort_dictionary.py
@@ -0,0 +1,24 @@
+"""
+Given marks scored out of 100 by a student in subjects where name of the subject is key and marks scored is the value. Our task is to sort the hashmap according to values i.e. according to marks.
+
+Input : Key = Math, Value = 98
+        Key = Data Structure, Value = 85
+        Key = Database, Value = 91
+        Key = Java, Value = 95
+        Key = Operating System, Value = 79
+        Key = Networking, Value = 80
+
+Output : Key = Operating System, Value = 79
+         Key = Networking, Value = 80
+         Key = Data Structure, Value = 85
+         Key = Database, Value = 91
+         Key = Java, Value = 95
+         Key = Math, Value = 98
+
+"""
+def sort_dictionary(keys):
+
+
+
+sort_dictionary(['greeting','hi','hello','hey', 'hai'])
+
