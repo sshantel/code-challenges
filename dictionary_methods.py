44ce04a9b85459a2ad14244b2e9afabbe3dd6fe6
Chantel Yip <chantelyip422@gmail.com>
2021-04-14 18:58:11 -0700

    added clear, copy, fromkeys

--- dictionary_methods.py
+++ dictionary_methods.py
@@ -1,3 +1,27 @@
 """ 
 Playing with dictionary methods
 """ 
+def dict_methods(dict):
+
+#clear() - Removes all elements from dictionary
+    dict.clear()
+# copy()	Returns a copy of the dictionary
+    dict.copy()
+# fromkeys()	Returns a dictionary with the specified keys and value
+    x = ('cheese', 'bread', 'cake')
+    y = 0
+    new_dict = dict.fromkeys(x, y)
+    print(new_dict)
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the 
+
+
+dict_methods({1:2,3:4,5:6})
+dict_methods({'dog':5, 'cat':1, 'monkey': 2, 'baboon': 6})
+
