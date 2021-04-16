f16be15eb7df528ba7ed2bd7211c3d6271934604
Chantel Yip <chantelyip422@gmail.com>
2021-04-15 23:25:40 -0700

    initialized ttt board

--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -3,9 +3,22 @@ Building a Tic Tac Toe Class
 """
 
 class TicTacToe():
-    def __init__(self, size):
-        self.size = size 
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns 
 
+    def display_board(self):
+        matrix = []
+        for i in range(self.rows): 
+            board = []
+            for j in range(self.columns): 
+                board.append([0])
+        print(board)
+        matrix.append(board)
+        print(matrix)
+        
 
-tt = TicTacToe(3)
-print(tt)
+tt = TicTacToe(3, 3)
+print(tt) 
+print(tt.display_board) 
+print(tt.columns) 
