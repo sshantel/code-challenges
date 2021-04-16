fb9f124a8384d733e55e4606d5e469e097b188ca
Chantel Yip <chantelyip422@gmail.com>
2021-04-15 23:51:16 -0700

    initialized matrix inside constructor

--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -6,19 +6,12 @@ class TicTacToe():
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns 
+        self.matrix = [[0 for i in range(rows)] for j in range(columns)]
 
-    def display_board(self):
-        matrix = []
-        for i in range(self.rows): 
-            board = []
-            for j in range(self.columns): 
-                board.append([0])
-        print(board)
-        matrix.append(board)
-        print(matrix)
-        
-
+    def display_board(self): 
+        return self.matrix
 tt = TicTacToe(3, 3)
 print(tt) 
-print(tt.display_board) 
-print(tt.columns) 
+print(tt.display_board()) 
+print(tt.columns)
+print(tt.display_board()) 
