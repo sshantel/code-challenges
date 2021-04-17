88e3bbddcb41ee368917441655ea7b17168636e2
Chantel Yip <chantelyip422@gmail.com>
2021-04-17 12:17:15 -0700

    checking for winner

--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -15,8 +15,16 @@ class TicTacToe():
         self.matrix[i][j] = player
         return self.matrix
 
+    def check_winner_row_column(self):
+        for row in self.matrix:
+            print(row)
+
+    def check_winner_diagnol(self):
+        pass
+
 tt = TicTacToe(3, 3) 
-print(tt.display_board())  
-tt.edit_board(0,0) 
+print(tt.display_board()) 
 print(tt.edit_board('x',0,0))
 print(tt.display_board())
+print(tt.edit_board('x', 0, 1))
+print(tt.edit_board('x', 0, 2))
