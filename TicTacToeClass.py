1ef38f3cec114fabb084af99a1aa208de18cff1b
Chantel Yip <chantelyip422@gmail.com>
2021-04-17 15:29:14 -0700

    fixes for column

--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -1,5 +1,7 @@
 """
 Building a Tic Tac Toe Class
+
+-check if coordinates are out of bounds
 """
 
 
@@ -28,19 +30,25 @@ class TicTacToe():
 
     def check_winner_row(self):
         for row in self.matrix:
-            if len(set(row)) == 1:
-                return (f'row winner! Winner is {row[0]}')
+            if '_' not in row:
+                if len(set(row)) == 1:
+                    return (f'row winner! Winner is {row[0]}')
+            else:
+                return ('no winner yet!')
+
+    def check_winner_column(self):
+        result = []
+        for row in range(len(self.matrix)):
+            for column in range(len(self.matrix[row])):
+                print(self.matrix[column])
 
     def check_winner_diagnol(self):
         diagnols = []
         for row in range(len(self.matrix)):
-            print(f'row is {row}')
             for column in range(len(self.matrix[0])):
-                print(f'column is {column}')
                 if (row == column) and self.matrix[row][column] != '_':
                     diagnols.append([row, column])
                     if len(diagnols) == len(self.matrix):
-                        print(diagnols)
                         return (
                             f'diagnol winner! Winner is {self.matrix[row][column]}'
                         )
@@ -49,7 +57,8 @@ class TicTacToe():
 tt = TicTacToe(3, 3)
 print(tt.display_board())
 print(tt.edit_board('x', 0, 0))
-print(tt.edit_board('x', 1, 1))
-print(tt.edit_board('x', 2, 2))
+print(tt.edit_board('x', 1, 0))
+print(tt.edit_board('x', 2, 0))
 print(tt.check_winner_row())
+print(tt.check_winner_column())
 print(tt.check_winner_diagnol())
