ad8e91ba208057ffa4ac22346f4cd02099608c6b
Chantel Yip <chantelyip422@gmail.com>
2021-04-17 14:20:44 -0700

    fixed diagnol function to accept the columns

--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -2,21 +2,24 @@
 Building a Tic Tac Toe Class
 """
 
+
 class TicTacToe():
     def __init__(self, rows, columns):
         self.rows = rows
-        self.columns = columns 
-        self.matrix = [[ '_' for i in range(rows)] for j in range(columns)]
+        self.columns = columns
+        self.matrix = [['_' for i in range(rows)] for j in range(columns)]
 
-    def display_board(self): 
+    def display_board(self):
         return self.matrix
 
-    def edit_board(self, player, i, j): 
+    def edit_board(self, player, i, j):
 
         if self.matrix[i][j] == '_':
             self.matrix[i][j] = player
         elif self.matrix[i][j] != '_':
-            print('sorry, this spot has already been taken. please choose a different spot') 
+            print(
+                'sorry, this spot has already been taken. please choose a different spot'
+            )
 
         display_tt = []
         for row in self.matrix:
@@ -25,21 +28,28 @@ class TicTacToe():
 
     def check_winner_row(self):
         for row in self.matrix:
-            print(f'row is {row}')
             if len(set(row)) == 1:
-                return(f'row winner! Winner is {row[0]}') 
+                return (f'row winner! Winner is {row[0]}')
 
     def check_winner_diagnol(self):
-        for row in self.matrix:
-            for column in row:
-                if len(set(column)) == 1:
-                    return (f'diagnol winner! Winner is {column[0]}')
- 
+        diagnols = []
+        for row in range(len(self.matrix)):
+            print(f'row is {row}')
+            for column in range(len(self.matrix[0])):
+                print(f'column is {column}')
+                if (row == column):
+                    diagnols.append(self.matrix[row][column])
+                    if len(diagnols) == len(self.matrix):
+                        print(diagnols)
+                        return (
+                            f'diagnol winner! Winner is {self.matrix[row][column]}'
+                        )
 
-tt = TicTacToe(3, 3) 
-print(tt.display_board())   
-print(tt.edit_board('x', 0, 0)) 
+
+tt = TicTacToe(3, 3)
+print(tt.display_board())
+print(tt.edit_board('x', 0, 0))
 print(tt.edit_board('x', 1, 1))
-print(tt.edit_board('x', 2,2)) 
-print(tt.check_winner_row()) 
-print(tt.check_winner_diagnol()) 
+print(tt.edit_board('x', 1, 2))
+print(tt.check_winner_row())
+print(tt.check_winner_diagnol())
